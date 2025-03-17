1d1b7a7288cd0fe0cb67a9245d80371a1ec03246
Narhwalsmart <shaun_adriano@dlsu.edu.ph>
2025-03-17 09:10:42 +0800

    Update view_students.py

--- view_students.py
+++ view_students.py
@@ -9,6 +9,6 @@ def view_students():
                 return
             print("\nAll Student Records:")
             for student in students:
-                print(f"ID: {student['id']}, Name: {student['name']}, Age: {student['age']}, Course: {student['course']}")
+                print(f"ID: {student['id']}, Name: {student['name']}, Age: {student['age']}, Course: {student['course']}, Current grade: {student['average_grade']}")
     except (FileNotFoundError, json.JSONDecodeError):
         print("\nNo student records found.")
